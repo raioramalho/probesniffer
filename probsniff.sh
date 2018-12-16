dac56ca2b2a65da05438c3f66d590d23d1573e78
Alan Ramalho <ramalho.sit@gmail.com>
2018-12-16 20:57:35 -0200

    Update probsniff.sh

--- probsniff.sh
+++ probsniff.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+# encoding utf-8
+# Created by: Acr4n1us
+# Contact & Changelog: https://github.com/RamalhoSec
+# Bitcoin: 3DppKRbA9Um3z4wnmVtkqnETnvwsip7WkC
+# Version 0.1
+
+
+#::set color palet
+red='\e[91m'
+green='\e[92m'
+cyan='\e[96m'
+blue='\e[34m'
+yellow='\e[33m'
+end='\e[50m'
