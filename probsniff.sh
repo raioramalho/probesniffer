ab877e8dd4157fe35986e3d52b1a9d54bd128f72
Alan Ramalho <ramalho.sit@gmail.com>
2018-12-16 22:13:49 -0200

    Update probsniff.sh

--- probsniff.sh
+++ probsniff.sh
@@ -51,7 +51,7 @@ MIRROR=$(cat /tmp/probsniff.check | grep "VERSION=" | sed 's/VERSION=//' | sed '
 
 
 #::set global functions
-F_CHANNEL_HOP() {
+channel_hop() {
   IEEE80211bg="1 2 3 4 5 6 7 8 9 10 11"
 	IEEE80211bg_intl="$IEEE80211b 12 13 14"
 	IEEE80211a="36 40 44 48 52 56 60 64 149 153 157 161"
